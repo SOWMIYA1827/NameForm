9dfb6387f737f39e4b4c3e4a40e54cf50d19f4e8
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-06-09 18:24:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template, redirect , request
-import sqlite3
+from flask import Flask, render_template, request, redirect
+import psycopg2
+import os
 
 app = Flask(__name__)
 
+def get_db_connection():
+    return psycopg2.connect(
+        host=os.environ.get("DB_HOST"),
+        database=os.environ.get("DB_NAME"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        port=os.environ.get("DB_PORT", "5432")
+    )
+
 def init_db():
-    conn = sqlite3.connect("feedback.db")
+    conn = get_db_connection()
     cursor = conn.cursor()
 
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS feedback (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id SERIAL PRIMARY KEY,
         name TEXT,
         rno TEXT,
         dept TEXT,
@@ -22,9 +32,9 @@ def init_db():
     """)
 
     conn.commit()
+    cursor.close()
     conn.close()
 
-# Create table when app starts
 init_db()
 
 @app.route("/", methods=["GET", "POST"])
@@ -32,25 +42,26 @@ def index():
 
     if request.method == "POST":
 
-        name = request.form["name"]
-        rno = request.form["rno"]
-        dept = request.form["dept"]
-        year = request.form["year"]
-        domain = request.form["domain"]
-        experience = request.form["experience"]
-        skills = request.form["skills"]
-        specific = request.form["specific"]
-
-        conn = sqlite3.connect("feedback.db")
+        conn = get_db_connection()
         cursor = conn.cursor()
 
         cursor.execute("""
         INSERT INTO feedback
         (name, rno, dept, year, domain, experience, skills, specific)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-        """, (name, rno, dept, year, domain, experience, skills, specific))
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        """, (
+            request.form["name"],
+            request.form["rno"],
+            request.form["dept"],
+            request.form["year"],
+            request.form["domain"],
+            request.form["experience"],
+            request.form["skills"],
+            request.form["specific"]
+        ))
 
         conn.commit()
+        cursor.close()
         conn.close()
 
         return redirect("/view")
@@ -59,11 +70,22 @@ def index():
 
 @app.route("/view")
 def view():
-    conn = sqlite3.connect("feedback.db")
+
+    conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM feedback")
+
+    cursor.execute("""
+        SELECT id, name, rno, dept, year,
+               domain, experience, skills, specific
+        FROM feedback
+        ORDER BY id DESC
+    """)
+
     data = cursor.fetchall()
+
+    cursor.close()
     conn.close()
+
     return render_template("view.html", data=data)
 
 if __name__ == "__main__":
